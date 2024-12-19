31e4b31bb53e522b6dc9c1e45bf88c2a4e2e6004
daftmaple <ubuntumaple@gmail.com>
2024-12-20 01:31:51 +1100

    Add delay to shoutout

--- src/module/output/shoutout.ts
+++ src/module/output/shoutout.ts
@@ -1,10 +1,19 @@
 import type { BaseApiClient } from "@twurple/api/lib/client/BaseApiClient";
 
+import { wait } from "../../helpers";
+import { type ShoutoutOutput } from "../../validator";
 import type { MessageScope } from "../type";
 
 export const shoutoutOutputHandler = async (
   apiClient: BaseApiClient,
-  { channel, user }: MessageScope
+  { channel, user }: MessageScope,
+  outputTrigger: ShoutoutOutput
 ) => {
+  const { delay } = outputTrigger;
+
+  if (delay) {
+    await wait(delay);
+  }
+
   await apiClient.chat.shoutoutUser(channel.id, user.id);
 };
